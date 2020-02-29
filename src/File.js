d2c206ed0037e4caba3882d19a6a77414802f65c
Alex <pkms1@yandex.ru>
2020-02-29 17:57:07 +0300

    Add new uploading  form to File.js

--- src/File.js
+++ src/File.js
@@ -5,29 +5,21 @@ export class File extends Component {
     render(){
         return (
             <div>
-                <form method="POST" action="/upload" enctype="multipart/form-data">
-                <input type="file" name="file" /><br/>
-               <input type="submit" value="Submit" />
-            </form>
-
-             /*   <link rel="stylesheet"
-                href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css"/>
-                    <div className="example-2">
-                        <div className="form-group"  >
-                            <input type="file" name="file" id="file" className="input-file" ></input>
-                                <label htmlFor="file" className="btn btn-tertiary js-labelFile">
-                                    <i className="icon fa fa-check"/>
-                                    <span className="js-fileName">Загрузить файл</span>
-                                    <input type="submit" value="Submit" />
-                                </label>
-                        </div>
-                    </div>*/
-
+                <form method="POST" encType="multipart/form-data" action="/u">
+                    <table>
+                        <tr>
+                            <td>File to upload:</td>
+                            <td><input type="file" name="file"/></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><input type="submit" value="Upload"/></td>
+                        </tr>
+                    </table>
+                </form>
             </div>
         )
     }
-
-
 }
 
 export default File
