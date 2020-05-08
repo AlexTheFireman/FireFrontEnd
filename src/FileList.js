c06360603d0c6c73c8bb4019f51587b89517d1d5
Alex <pkms1@yandex.ru>
2020-05-08 23:49:48 +0300

    Add Link to const body to get clickable list of files in FileList

--- src/FileList.js
+++ src/FileList.js
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
-import Table  from './Table.js';
-import {Route} from "react-router-dom";
-import Link from "react-router-dom/modules/Link";
-import Switch from "react-bootstrap/cjs/Switch";
+import { Link } from 'react-router-dom';
 
 export default function FileList () {
     const [files, setFiles] = useState([]);
 
     useEffect((api) => {
+
         fetch("/api/get/all")
             .then(response => response.json())
             .then(data => {
@@ -15,37 +13,12 @@ export default function FileList () {
             });
     }, []);
 
-    const FilesAPI = {
-        files,
-        all: function() { return this.files },
-        get: function(name) {
-            const isFile = f => f.fileName === name
-            return this.files.find(isFile)
-        }
-
-    };
-
-    const FullList = () => (
-        <div>
-            <ul>
-                {
-                    FilesAPI.all().map(f => (
-                        <li key={f.fileName}>
-                            <Link to={'/tables/${f.fileName}'}>{f.fileName}</Link>/>
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
-
-    );
-
-    const body = () => (
-        <Switch>
-            <Route exact path='/fileList' component={FullList}/>
-            <Route path='/tables/:fileName' component={Table}/>
-        </Switch>
-    );
+    const body = files.map((file) => {
+        return (
+            <tr key={file}>
+                <td><Link to={`/tables/${file}`}>{file}</Link></td>
+            </tr>)
+    })
 
     return (
         <table className="table table-bordered table-sm table-hover table-striped">
@@ -59,12 +32,8 @@ export default function FileList () {
             </tbody>
         </table>
     );
+
 }
-
-
-
-
-
 
 
 
