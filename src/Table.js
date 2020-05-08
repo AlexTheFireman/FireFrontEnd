05129d2080de7ebfb9b0fc9f994ad05b7efcd83f
Alex <pkms1@yandex.ru>
2020-05-08 23:52:24 +0300

    In Table.js add props.match.params and change feach request to get props from FileList.js

--- src/Table.js
+++ src/Table.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
-export default function Table () {
+export default function Table (props) {
     const [fires, setFires] = useState ([]);
 
-    useEffect((api) => {
-        fetch("/api/get/${fileName}")
-            .then(response => response.json())
-            .then(data => {
-                setFires(data);
-            });
-    }, []);
+
+        useEffect((api) => {
+            let name = props.match.params.name;
+            fetch('/api/get/' + name)
+                .then(response => response.json())
+                .then(data => {
+                    setFires(data);
+                });
+        }, []);
 
     const body = fires.map((fire, i) => {
-        return (
-            <tr key={i}>
+        return ( <tr key={i}>
                 <td className="bordered">{fire.id}</td>
                 <td className="bordered">{fire.date}</td>
                 <td className="bordered">{fire.message}</td>
@@ -26,18 +27,18 @@ export default function Table () {
     return (
         <table className="table table-bordered table-sm table-hover table-striped">
             <thead className="thead-inverse">
-            <tr>
-                <th className="bordered">№</th>
-                <th className="bordered">Дата</th>
-                <th className="bordered">Сообщение</th>
-                <th className="bordered">Адрес, объект пожара</th>
-                <th className="bordered">Район выезда</th>
-                <th className="bordered">Подразделение</th>
-            </tr>
+                <tr>
+                    <th className="bordered">№</th>
+                    <th className="bordered">Дата</th>
+                    <th className="bordered">Сообщение</th>
+                    <th className="bordered">Адрес, объект пожара</th>
+                    <th className="bordered">Район выезда</th>
+                    <th className="bordered">Подразделение</th>
+                </tr>
             </thead>
-            <tbody>
-            {body}
-            </tbody>
+                <tbody>
+                    {body}
+                </tbody>
         </table>
     );
 }
