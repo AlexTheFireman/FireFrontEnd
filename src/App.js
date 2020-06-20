564f2f805fc7fa4b92353b11f48729a8b71d1161
Alex <pkms1@yandex.ru>
2020-06-20 19:30:49 +0300

    add Route for FilteredTable component

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import MyNavbar from './MyNavbar.js';
 import File from './File.js';
 import BigTable from './BigTable.js';
 import FileList from './FileList.js';
-import POSTTable from "./POSTTable.js";
+import POSTTable from './POSTTable.js';
+import FilterForm from './FilterForm.js';
+import FilteredTable from './FilteredTable';
 
 export default function App() {
     return (
@@ -18,10 +20,12 @@ export default function App() {
             <div>
                 <MyNavbar/>
                <Switch>
+                    <Route path='/filterForm' component={FilterForm}/>
                     <Route path='/file' component={File}/>
                     <Route path='/tables/:name' component={POSTTable}/>
                     <Route path='/big' component={BigTable}/>
                     <Route path='/fileList' component={FileList}/>
+                    <Route path='/tables/:name' component={FilteredTable}/>
                 </Switch>
             </div>
         </BrowserRouter>
