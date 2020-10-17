d5c7c2e07cd2734cf7733f4ac13b308c2977c161
AlexTheFireman <bizhov.alexander@gmail.com>
2020-10-17 20:39:53 +0300

    Change targen in setupProxy.js

--- src/File.js
+++ src/File.js
@@ -1,23 +1,68 @@
-import React from "react";
+import React, {Component} from "react";
 
-export default function File (props) {
+class File extends Component {
+    constructor(props) {
+        super(props);
+    }
+    state = {
+        selectedFile: null,
+        responseFromServer: null
+    }
 
-    return (
-        <div align="center">
-            <form method="POST" encType="multipart/form-data" action="/api/upload">
+    fileSelectedHandler = event => {
+        this.setState({
+            selectedFile: event.target.files[0]
+        })
+    }
+
+    fileUploadHandler = () => {
+        const formData = new FormData();
+        formData.append('file', this.state.selectedFile);
+        fetch("/api/upload", {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.text())
+            .then(response=>this.setState({responseFromServer:response})
+
+            )}
+
+    render() {
+        const status = this.state.responseFromServer;
+        let warning;
+
+        if (status === "Check") {
+            warning = <h1>Принимаются файлы только с расширением "xlsx" или "xls"</h1>;
+        } else if (status === "FileAlreadyHas") {
+            warning = <h1>Файл с таким именем уже существует в базе данных</h1>;
+        } else if (status === "Check") {
+            warning = <h1>Проверьте, пожалуйста, расширение файла</h1>;
+        } else if (status === "Success") {
+            warning = <h1>Файл успешно загружен</h1>;
+        } else {
+            warning = <h1>Файл не выбран</h1>;
+        }
+
+        return (
+            <div align="center">
                 <table>
                     <tr>
                         <td>Выберите файл:</td>
-                        <td><input type="file" name="file"/></td>
+                        <td><input type="file" onChange={this.fileSelectedHandler}/></td>
                     </tr>
                     <tr>
                         <td></td>
-                        <td><input type="submit" value="Загрузить"/></td>
+                        <td><button onClick={this.fileUploadHandler}>Загрузить</button></td>
                     </tr>
                 </table>
-            </form>
-        </div>
-    )
+                <td>{warning}</td>
+            </div>
+        );
+    }
 }
+
+export default File
+
+
 
 
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const target = 'http://localhost:9090';
+const target = 'https://acc-frontend.herokuapp.com/';
+
+//const target = 'http://localhost:9090';
 
 module.exports = app => {
     app.use(createProxyMiddleware('/api/**', { target }));
