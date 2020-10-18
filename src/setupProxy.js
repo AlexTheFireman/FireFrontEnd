10ebc214593d8d4e9a290d163447dbb97fe4e87f
AlexTheFireman <bizhov.alexander@gmail.com>
2020-10-18 23:29:01 +0300

    Change setupProxy package.json package-lock.json .gitignore

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,8 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const target = 'https://acc-frontend.herokuapp.com';
-
-//const target = 'http://localhost:9090';
+const target = 'http://localhost:9090';
 
 module.exports = app => {
     app.use(createProxyMiddleware('/api/**', { target }));
