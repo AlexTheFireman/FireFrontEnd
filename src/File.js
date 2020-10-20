e493ad50e58d0e9514e5bace38ca00cb8e8025c9
AlexTheFireman <bizhov.alexander@gmail.com>
2020-10-21 01:09:27 +0300

    Try to add switch with cases from server.

--- src/File.js
+++ src/File.js
@@ -4,16 +4,17 @@ class File extends Component {
     constructor(props) {
         super(props);
     }
+
     state = {
         selectedFile: null,
         responseFromServer: null
-    }
+    };
 
     fileSelectedHandler = event => {
         this.setState({
             selectedFile: event.target.files[0]
         })
-    }
+    };
 
     fileUploadHandler = () => {
         const formData = new FormData();
@@ -23,43 +24,55 @@ class File extends Component {
             body: formData
         })
             .then(response => response.text())
-            .then(response=>this.setState({responseFromServer:response})
-
-            )}
+            .then(response => this.setState({responseFromServer: response})
+            )
+    };
 
     render() {
-        const status = this.state.responseFromServer;
+        const status = {
+            FILE_ALREADY_EXIST: "Файл с таким именем уже существует в базе данных",
+            SUCCESS: 'winter',
+            CHECK_FILE_EXTENSION: 'spring',
+            NO_FILE_SELECTED: 'autumn'
+        };
+
         let warning;
 
-        if (status === "Check") {
-            warning = <h1>Принимаются файлы только с расширением "xlsx" или "xls"</h1>;
-        } else if (status === "FileAlreadyHas") {
-            warning = <h1>Файл с таким именем уже существует в базе данных</h1>;
-        } else if (status === "Check") {
-            warning = <h1>Проверьте, пожалуйста, расширение файла</h1>;
-        } else if (status === "Success") {
-            warning = <h1>Файл успешно загружен</h1>;
-        } else {
-            warning = <h1>Файл не выбран</h1>;
+        switch (this.state.responseFromServer) {
+            case status.FILE_ALREADY_EXIST:
+                warning = <h1>status.FILE_ALREADY_EXIST</h1>;
+                break;
+            case status.SUCCESS:
+                warning = <h1>Файл успешно загружен</h1>;
+                break;
+            case status.CHECK_FILE_EXTENSION:
+                warning = <h1>Принимаются файлы только с расширением "xlsx" или "xls".</h1>;
+                break;
+            case status.NO_FILE_SELECTED:
+                warning = <h1>Файл не выбран</h1>;
+                break;
+            default:
+                break;
+            }
+                return (
+                    <div align="center">
+                        <table>
+                            <tr>
+                                <td>Выберите файл:</td>
+                                <td><input type="file" onChange={this.fileSelectedHandler}/></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>
+                                    <button onClick={this.fileUploadHandler}>Загрузить</button>
+                                </td>
+                            </tr>
+                        </table>
+                        <td>{warning}</td>
+                    </div>
+                );
         }
-
-        return (
-            <div align="center">
-                <table>
-                    <tr>
-                        <td>Выберите файл:</td>
-                        <td><input type="file" onChange={this.fileSelectedHandler}/></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td><button onClick={this.fileUploadHandler}>Загрузить</button></td>
-                    </tr>
-                </table>
-                <td>{warning}</td>
-            </div>
-        );
     }
-}
 
 export default File
 
