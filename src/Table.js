d7d6e6d2b44bcb5f7db72fe741c540ffa3a53ece
AlexTheFireman <bizhov.alexander@gmail.com>
2020-10-28 10:53:15 +0300

    Delete classes in <td> and <th> elements. Set style for them in <tr> elements.

--- src/Table.js
+++ src/Table.js
@@ -6,84 +6,84 @@ import React from 'react';
 
 export default function Table ({fires}) {
     const body = fires.map((fire, i) => {
-        return <tr key={i}>
-            <td className="bordered">{fire.id}</td>
-            <td className="bordered">{fire.date}</td>
-            <td className="bordered">{fire.message}</td>
-            <td className="bordered">{fire.addressObjectFireFeature}</td>
-            <td className="bordered">{fire.district}</td>
-            <td className="bordered">{fire.fireStation}</td>
-            <td className="bordered">{fire.object1}</td>
-            <td className="bordered">{fire.object2}</td>
-            <td className="bordered">{fire.rescueWorks}</td>
-            <td className="bordered">{fire.amountOfRescuedPeople}</td>
-            <td className="bordered">{fire.amountOfEvacuatedPeople}</td>
-            <td className="bordered">{fire.fireChiefRank}</td>
-            <td className="bordered">{fire.fireChiefName}</td>
-            <td className="bordered">{fire.amountOfSmokeGroups}</td>
-            <td className="bordered">{fire.smokeTime}</td>
-            <td className="bordered">{fire.extinguishingAgents}</td>
-            <td className="bordered">{fire.firstEngine}</td>
-            <td className="bordered">{fire.secondEngine}</td>
-            <td className="bordered">{fire.thirdEngine}</td>
-            <td className="bordered">{fire.firstReserve}</td>
-            <td className="bordered">{fire.secondReserve}</td>
-            <td className="bordered">{fire.firstSquadron}</td>
-            <td className="bordered">{fire.secondSquadron}</td>
-            <td className="bordered">{fire.hydrant}</td>
-            <td className="bordered">{fire.reportPDF}</td>
-            <td className="bordered">{fire.locality}</td>
-            <td className="bordered">{fire.fireRank}</td>
-            <td className="bordered">{fire.detectionTime}</td>
-            <td className="bordered">{fire.messageTime}</td>
-            <td className="bordered">{fire.arrivalTime}</td>
-            <td className="bordered">{fire.firstNozzleTime}</td>
-            <td className="bordered">{fire.localizationTime}</td>
-            <td className="bordered">{fire.fireLiquidationTime}</td>
-            <td className="bordered">{fire.totalLiquidationTime}</td>
-            <td className="bordered">{fire.comment}</td>
+        return <tr className="bordered" key={i}>
+            <td>{fire.id}</td>
+            <td>{fire.date}</td>
+            <td>{fire.message}</td>
+            <td>{fire.addressObjectFireFeature}</td>
+            <td>{fire.district}</td>
+            <td>{fire.fireStation}</td>
+            <td>{fire.object1}</td>
+            <td>{fire.object2}</td>
+            <td>{fire.rescueWorks}</td>
+            <td>{fire.amountOfRescuedPeople}</td>
+            <td>{fire.amountOfEvacuatedPeople}</td>
+            <td>{fire.fireChiefRank}</td>
+            <td>{fire.fireChiefName}</td>
+            <td>{fire.amountOfSmokeGroups}</td>
+            <td>{fire.smokeTime}</td>
+            <td>{fire.extinguishingAgents}</td>
+            <td>{fire.firstEngine}</td>
+            <td>{fire.secondEngine}</td>
+            <td>{fire.thirdEngine}</td>
+            <td>{fire.firstReserve}</td>
+            <td>{fire.secondReserve}</td>
+            <td>{fire.firstSquadron}</td>
+            <td>{fire.secondSquadron}</td>
+            <td>{fire.hydrant}</td>
+            <td>{fire.reportPDF}</td>
+            <td>{fire.locality}</td>
+            <td>{fire.fireRank}</td>
+            <td>{fire.detectionTime}</td>
+            <td>{fire.messageTime}</td>
+            <td>{fire.arrivalTime}</td>
+            <td>{fire.firstNozzleTime}</td>
+            <td>{fire.localizationTime}</td>
+            <td>{fire.fireLiquidationTime}</td>
+            <td>{fire.totalLiquidationTime}</td>
+            <td>{fire.comment}</td>
         </tr>
     });
 
     return (
-        <table className="table table-bordered table-sm table-hover table-striped">
-            <thead className="thead-inverse">
-            <tr>
-                <th className="bordered">№</th>
-                <th className="bordered">Дата</th>
-                <th className="bordered">Назначение сообщения</th>
-                <th className="bordered">Адрес, наименование объекта и характер (пожара, загорания, происшествия), S=пожара</th>
-                <th className="bordered">Район выезда</th>
-                <th className="bordered">Подразделение</th>
-                <th className="bordered">Объект назначения</th>
-                <th className="bordered">Из них объект назначения</th>
-                <th className="bordered">Проведение аварийно спасательных работ</th>
-                <th className="bordered">Спасено людей (Деблокировано)</th>
-                <th className="bordered">Эвакуировано</th>
-                <th className="bordered">Руководитель тушения пожара (Руководитель ликвидации аварии)</th>
-                <th className="bordered">ФИО</th>
-                <th className="bordered">Использование ГДЗС</th>
-                <th className="bordered">Время работы ГДЗС (без надписи мин.)</th>
-                <th className="bordered">Применение огнетушащих веществ</th>
-                <th className="bordered">1 отделение</th>
-                <th className="bordered">2 отделение</th>
-                <th className="bordered">3 отделение</th>
-                <th className="bordered">Резерв 1 (АМГ, ОП)</th>
-                <th className="bordered">Резерв 2 (АМГ, ОП)</th>
-                <th className="bordered">АМГ, ОП</th>
-                <th className="bordered">АМГ, (ОП)</th>
-                <th className="bordered">Использование ИППВ</th>
-                <th className="bordered">Донесение о пожаре формат *.pdf</th>
-                <th className="bordered">Село/город</th>
-                <th className="bordered">Ранг</th>
-                <th className="bordered">Время обнаружения</th>
-                <th className="bordered">Время сообщения</th>
-                <th className="bordered">Время прибыти</th>
-                <th className="bordered">Время подачи первого ствола</th>
-                <th className="bordered">Время локализации с момента подачи первого ствола</th>
-                <th className="bordered">Время ликвидации с момента локализации</th>
-                <th className="bordered">Время ликвидации последствий пожара (учитывая проведение спец работ)</th>
-                <th className="bordered">Примечание</th>
+        <table className="thead-inverse table-sm table-hover table-striped">
+            <thead   >
+            <tr className="bordered">
+                <th>№</th>
+                <th>Дата</th>
+                <th>Назначение сообщения</th>
+                <th>Адрес, наименование объекта и характер (пожара, загорания, происшествия), S=пожара</th>
+                <th>Район выезда</th>
+                <th>Подразделение</th>
+                <th>Объект назначения</th>
+                <th>Из них объект назначения</th>
+                <th>Проведение аварийно спасательных работ</th>
+                <th>Спасено людей (Деблокировано)</th>
+                <th>Эвакуировано</th>
+                <th>Руководитель тушения пожара (Руководитель ликвидации аварии)</th>
+                <th>ФИО</th>
+                <th>Использование ГДЗС</th>
+                <th>Время работы ГДЗС (без надписи мин.)</th>
+                <th>Применение огнетушащих веществ</th>
+                <th>1 отделение</th>
+                <th>2 отделение</th>
+                <th>3 отделение</th>
+                <th>Резерв 1 (АМГ, ОП)</th>
+                <th>Резерв 2 (АМГ, ОП)</th>
+                <th>АМГ, ОП</th>
+                <th>АМГ, (ОП)</th>
+                <th>Использование ИППВ</th>
+                <th>Донесение о пожаре формат *.pdf</th>
+                <th>Село/город</th>
+                <th>Ранг</th>
+                <th>Время обнаружения</th>
+                <th>Время сообщения</th>
+                <th>Время прибыти</th>
+                <th>Время подачи первого ствола</th>
+                <th>Время локализации с момента подачи первого ствола</th>
+                <th>Время ликвидации с момента локализации</th>
+                <th>Время ликвидации последствий пожара (учитывая проведение спец работ)</th>
+                <th>Примечание</th>
             </tr>
             </thead>
             <tbody>
