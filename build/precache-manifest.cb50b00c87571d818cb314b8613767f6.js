f5cba6c3939be224b91930044ccf32f1e39ceb58
Alex-fire <pkms1@yandex.ru>
2020-12-23 19:10:41 +0300

    Tryinf deploy to Heroku again

---
+++ build/precache-manifest.cb50b00c87571d818cb314b8613767f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3062820f225701e9103bb43ec72dbf23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b61003221fcb2594fe0",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "150e4d9587fa209b331a",
+    "url": "/static/css/main.e4d48b56.chunk.css"
+  },
+  {
+    "revision": "0b61003221fcb2594fe0",
+    "url": "/static/js/2.daf2550f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.daf2550f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "150e4d9587fa209b331a",
+    "url": "/static/js/main.7bd75bf6.chunk.js"
+  },
+  {
+    "revision": "2d177ac0df7bd91c1206",
+    "url": "/static/js/runtime-main.427d1c85.js"
+  }
+]);
