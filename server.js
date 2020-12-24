0aa66bb75ce6f3e73d45f1e9183d6f1e2e11656f
Alex-fire <pkms1@yandex.ru>
2020-12-24 12:27:01 +0300

    Add express dependency

--- server.js
+++ server.js
@@ -1,28 +1,7 @@
 var express = require('express');
 var app = express();
 const favicon = require('express-favicon');
-// const helmet = require('helmet')
-// app.use(
-//     // [
-//     helmet.contentSecurityPolicy({
-//         directives: {
-//             defaultSrc: ["'self'"],
-//             connectSrc: ["'self'", 'https://checkout.stripe.com'],
-//             frameSrc: ["'self'", 'https://checkout.stripe.com'],
-//             childSrc: ["'self'", 'https://checkout.stripe.com'],
-//             scriptSrc: ["'self'", 'https://checkout.stripe.com'],
-//             styleSrc: [
-//                 "'self'",
-//                 'https://fonts.googleapis.com',
-//                 'https://checkout.stripe.com',
-//             ],
-//             fontSrc: ["'self'", 'https://fonts.gstatic.com'],
-//             imgSrc: ["'self'", 'https://*.stripe.com', 'https://res.cloudinary.com'],
-//             baseUri: ["'self'"],
-//         },
-//     })
-//     // ]
-// );
+
 app.use(favicon(__dirname + '/build/favicon.ico'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
