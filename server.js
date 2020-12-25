52fcbb719e66de11903a6b8b38402f6afb747e68
Alex-fire <pkms1@yandex.ru>
2020-12-25 13:51:46 +0300

    Add engines to package.json. Add new code to server.js

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-var express = require('express');
-var app = express();
-const favicon = require('express-favicon');
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+// const bodyParser = require('body-parser');
+const path = require("path");
+const app = express();
+const port = process.env.PORT || 8080;
 
-app.use(favicon(__dirname + '/build/favicon.ico'));
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`);
-});
-app.use(express.static(__dirname + '/'));
-// // app.listen(process.env.axdvcPORT || 8080);
+app.use(express.static(path.join(__dirname, "build")));
+
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
+// var express = require('express');
+// var app = express();
+// const favicon = require('express-favicon');
+//
+// app.use(favicon(__dirname + '/build/favicon.ico'));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => {
+//     console.log(`server started on port ${PORT}`);
+// });
+// app.use(express.static(__dirname + '/'));
