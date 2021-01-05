066b497e06650102750d06b61e24a15a269e847a
Alex-fire <pkms1@yandex.ru>
2021-01-05 18:42:33 +0300

    New target in setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const target = 'https://back-for-app.herokuapp.com';
-
-module.exports = app => {
-    app.use(createProxyMiddleware('/api/**', { target }));
+module.exports = function(app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'https://back-for-app.herokuapp.com',
+            changeOrigin: true
+        })
+    );
 };
+// const target = 'https://back-for-app.herokuapp.com';
+//
+// module.exports = app => {
+//     app.use(createProxyMiddleware('/api/**', { target }));
+// };
