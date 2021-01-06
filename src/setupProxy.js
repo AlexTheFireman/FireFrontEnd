72d42141960cd3ffa45aa73cd22e7c785da4f9b7
Alex-fire <pkms1@yandex.ru>
2021-01-06 19:01:45 +0300

    Another try with target in setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,6 +1,16 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const target = 'https://back-for-app.herokuapp.com';
+//const target = 'https://back-for-app.herokuapp';
 
-module.exports = app => {
-    app.use(createProxyMiddleware('/api/**', { target }));
+module.exports = function(app) {
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'https://back-for-app.herokuapp.com',
+            changeOrigin: true,
+        })
+    );
 };
+//
+// module.exports = app => {
+//     app.use(createProxyMiddleware('/api/**', { target }));
+// };
