31c1a1726ec7f136f26ad134f47ade18a4e43381
Alex-fire <pkms1@yandex.ru>
2021-01-06 18:01:54 +0300

    New setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,15 +1,15 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
+//const target = 'https://back-for-app.herokuapp';
 
 module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://back-for-app.herokuapp.com',
-            changeOrigin: true
+            target: 'https://back-for-app.herokuapp',
+            changeOrigin: true,
         })
     );
 };
-// const target = 'https://back-for-app.herokuapp.com';
 //
 // module.exports = app => {
 //     app.use(createProxyMiddleware('/api/**', { target }));
