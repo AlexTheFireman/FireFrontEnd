8d69789b6f3ee330fe303be3490e8fa26e23d11c
Alex-fire <pkms1@yandex.ru>
2021-01-08 19:31:50 +0300

    Add localhost:9090 to setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-//const target = 'https://back-for-app.herokuapp';
+//const target = 'https://back-for-app.herokuapp.com';
 
 module.exports = function(app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://back-for-app.herokuapp.com',
+            target: 'http://localhost:9090',
             changeOrigin: true,
         })
     );
