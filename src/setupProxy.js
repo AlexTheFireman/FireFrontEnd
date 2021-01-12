d03ccb9ec0dc85fa6c7c737c7b29fa4d37f1e76c
Alex-fire <pkms1@yandex.ru>
2021-01-12 19:49:10 +0300

    Move middleware from devDependencies to dependencies

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-//const target = 'https://back-for-app.herokuapp.com';
+const target = 'https://back-for-app.herokuapp.com';
 
-module.exports = function(app) {
-    app.use(
-        '/api',
-        createProxyMiddleware({
-            target: 'http://localhost:9090',
-            changeOrigin: true,
-        })
-    );
+
+module.exports = app => {
+    app.use(createProxyMiddleware('/api/*', { target }));
 };
-//
-// module.exports = app => {
-//     app.use(createProxyMiddleware('/api/**', { target }));
-// };
