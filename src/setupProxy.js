a26c1dc785b0372d62edfdca34d249b1415af9d6
Alex-fire <pkms1@yandex.ru>
2021-01-16 01:59:53 +0300

    Change static.json and setupSetup.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const target = 'https://back-for-app.herokuapp.com';
-
-
+const target = 'https://back-for-app.herokuapp.com/';
 module.exports = app => {
-    app.use(createProxyMiddleware('/api/*', { target }));
+    app.use(createProxyMiddleware('/api/**', { target }));
 };
