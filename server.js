f2b4ae5ca7b2e85a87327c65004e32949bf5f284
Stanislav Biryukov <s.biryukovs@gmail.com>
2021-01-18 02:14:27 +0300

    static json update

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`);
+    console.log(`server started on port ${PORT}, go to http://localhost:3000/`);
 });
 app.use(express.static(__dirname + '/'));
